62c5c7411146df0aebe7fc4af0863d8dbf82ca47
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-07 02:44:28 -0300

    feat: Añadir recursividad en el menu Principal

--- TP_Parque/menu/mostrarMenuPrincipal.hpp
+++ TP_Parque/menu/mostrarMenuPrincipal.hpp
@@ -1,5 +1,6 @@
 #ifndef MOSTRAR_MENU_PRINCIPAL_H
 #define MOSTRAR_MENU_PRINCIPAL_H
+#include "utiles/outputs/imprimirTitulo.hpp"
 #include "utiles/outputs/imprimirConNum.hpp"
 #include "menu/mostrarMenuVisitantes.hpp"
 #include <iostream>
@@ -9,33 +10,39 @@ using namespace std;
 void mostrarMenuPrincipal(){
     int opcionElegida = 0;
 
-    imprimirConNumero(1,"Gestionar Visitantes");
-    imprimirConNumero(2,"Gestionar Juegos");
-    imprimirConNumero(3,"Generar Datos De Prueba");
-    imprimirConNumero(4,"Salir del programa");
-
-    while (opcionElegida < 1 || opcionElegida > 4){
-        cout << "Ingresa el numero de opcion elegida: "; cin >> opcionElegida; cout<<endl;
-        if (opcionElegida < 1 || opcionElegida > 4){
-            cout << "Numero incorrecto. Ingrese uno correcto.\n";
-        }
-    };
-
-    
-    switch (opcionElegida)
+    while (opcionElegida != -1)
     {
-    case 1:
-        mostrarMenuVisitantes();
-        break;
-    case 2:
-        /* Menu Juegos */
-        break;
-    case 3:
-        /* Generar Datos de Prueba */
-        break;
-    case 4:
-        cout << "\n¡Hasta luego! ;)\n";
-        break;
+        opcionElegida = 0;
+        imprimirTitulo("Menu Principal");
+        imprimirConNumero(1,"Gestionar Visitantes");
+        imprimirConNumero(2,"Gestionar Juegos");
+        imprimirConNumero(3,"Generar Datos De Prueba");
+        imprimirConNumero(4,"Salir del programa");
+
+        while (opcionElegida < 1 || opcionElegida > 4){
+            cout << "Ingresa el numero de opcion elegida: "; cin >> opcionElegida; cout<<endl;
+            if (opcionElegida < 1 || opcionElegida > 4){
+                cout << "Numero incorrecto. Ingrese uno correcto.\n";
+            }
+        };
+
+        
+        switch (opcionElegida)
+        {
+        case 1:
+            mostrarMenuVisitantes();
+            break;
+        case 2:
+            /* Menu Juegos */
+            break;
+        case 3:
+            /* Generar Datos de Prueba */
+            break;
+        case 4:
+            cout << "\nHasta luego ;)\n\n";
+            opcionElegida = -1;
+            break;
+        }
     }
 
     return;
