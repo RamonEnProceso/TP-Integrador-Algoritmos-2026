0a331faf3193d32e5c9f7726efe82d56ad552dfc
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-07 02:43:26 -0300

    feat: Añadir funcion para crear Titulos

---
+++ TP_Parque/utiles/outputs/imprimirTitulo.hpp
@@ -0,0 +1,12 @@
+#ifndef IMPRIMIR_TITULO_H
+#define IMPRIMIR_TITULO_H
+#include <iostream>
+
+using namespace std;
+
+void imprimirTitulo(const char texto[]){
+    cout << "----------" << texto << "----------" << "\n" ;
+    return;
+};
+
+#endif
