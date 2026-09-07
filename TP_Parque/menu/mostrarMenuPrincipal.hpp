8d807e1cf80321c8a2229c7fbeda1bea718bf88a
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-07 02:20:35 -0300

    feat: Añadir opción de temrinar programa al Menu principal y menu de Visitantes

--- TP_Parque/menu/mostrarMenuPrincipal.hpp
+++ TP_Parque/menu/mostrarMenuPrincipal.hpp
@@ -1,6 +1,7 @@
 #ifndef MOSTRAR_MENU_H
 #define MOSTRAR_MENU_H
 #include "utiles/outputs/imprimirConNum.hpp"
+#include "menu/mostrarMenuVisitantes.hpp"
 #include <iostream>
 
 using namespace std;
@@ -11,13 +12,31 @@ void mostrarMenuPrincipal(){
     imprimirConNumero(1,"Gestionar Visitantes");
     imprimirConNumero(2,"Gestionar Juegos");
     imprimirConNumero(3,"Generar Datos De Prueba");
-    
-    while (opcionElegida < 1 || opcionElegida > 3){
+    imprimirConNumero(4,"Salir del programa");
+
+    while (opcionElegida < 1 || opcionElegida > 4){
         cout << "Ingresa el numero de opcion elegida: "; cin >> opcionElegida; cout<<endl;
-        if (opcionElegida < 1 || opcionElegida > 3){
+        if (opcionElegida < 1 || opcionElegida > 4){
             cout << "Numero incorrecto. Ingrese uno correcto.\n";
         }
     };
+
+    
+    switch (opcionElegida)
+    {
+    case 1:
+        mostrarMenuVisitantes();
+        break;
+    case 2:
+        /* Menu Juegos */
+        break;
+    case 3:
+        /* Generar Datos de Prueba */
+        break;
+    case 4:
+        cout << "\n¡Hasta luego! ;)\n";
+        break;
+    }
 
     return;
 };
