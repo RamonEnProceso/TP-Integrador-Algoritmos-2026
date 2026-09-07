1c8f9f50e130fc6c9aa93d63040c849d3b6f6da3
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-07 02:44:57 -0300

    feat: Añadir recursividad en el menu Visitantes

--- TP_Parque/menu/mostrarMenuVisitantes.hpp
+++ TP_Parque/menu/mostrarMenuVisitantes.hpp
@@ -1,7 +1,7 @@
 #ifndef MOSTRAR_MENU_VISITANTES_H
 #define MOSTRAR_MENU_VISITANTES_H
+#include "utiles/outputs/imprimirTitulo.hpp"
 #include "utiles/outputs/imprimirConNum.hpp"
-#include "menu/mostrarMenuPrincipal.hpp"
 #include <iostream>
 
 using namespace std;
@@ -9,43 +9,47 @@ using namespace std;
 void mostrarMenuVisitantes(){
     int opcionElegida = 0;
 
-    imprimirConNumero(1,"Mostrar Visitantes");
-    imprimirConNumero(2,"Registrar Visitante");
-    imprimirConNumero(3,"Buscar Visitante (Pulsera / Nombre)");
-    imprimirConNumero(4,"Ver Historial de juegos de Visitante (Pulsera / Nombre)");
-    imprimirConNumero(5,"Eliminar Visitante");
-    imprimirConNumero(6,"Volver al Menú Principal");
-    
-    while (opcionElegida < 1 || opcionElegida > 6){
-        cout << "Ingresa el numero de opcion elegida: "; cin >> opcionElegida; cout<<endl;
-        if (opcionElegida < 1 || opcionElegida > 6){
-            cout << "Numero incorrecto. Ingrese uno correcto.\n";
-        }
-    };
-
-    switch (opcionElegida)
+    while (opcionElegida != -1)
     {
-    case 1:
-        /* Mostrar Visitantes */
-        break;
-    case 2:
-        /* Registrar Visitante */
-        break;
-    case 3:
-        /* Buscar Visitante (Pulsera / Nombre) */
-        break;    
-    case 4:
-        /* Ver Historial de juegos de Visitante (Pulsera / Nombre) */
-        break;
-    case 5:
-        /* Eliminar Visitante */
-        break;
-    case 6:
-        mostrarMenuPrincipal();
-        break;
-    }
+        opcionElegida = 0;
+        imprimirTitulo("Menu Visitantes");
+        imprimirConNumero(1,"Mostrar Visitantes");
+        imprimirConNumero(2,"Registrar Visitante");
+        imprimirConNumero(3,"Buscar Visitante (Pulsera / Nombre)");
+        imprimirConNumero(4,"Ver Historial de juegos de Visitante (Pulsera / Nombre)");
+        imprimirConNumero(5,"Eliminar Visitante");
+        imprimirConNumero(6,"Volver al Menu Principal");
+        
+        while (opcionElegida < 1 || opcionElegida > 6){
+            cout << "Ingresa el numero de opcion elegida: "; cin >> opcionElegida; cout<<endl;
+            if (opcionElegida < 1 || opcionElegida > 6){
+                cout << "Numero incorrecto. Ingrese uno correcto.\n";
+            }
+        };
 
-    return;
+        switch (opcionElegida)
+        {
+        case 1:
+            /* Mostrar Visitantes */
+            break;
+        case 2:
+            /* Registrar Visitante */
+            break;
+        case 3:
+            /* Buscar Visitante (Pulsera / Nombre) */
+            break;    
+        case 4:
+            /* Ver Historial de juegos de Visitante (Pulsera / Nombre) */
+            break;
+        case 5:
+            /* Eliminar Visitante */
+            break;
+        case 6:
+            cout << "Regresando... \n\n";
+            opcionElegida = 0;
+            return;
+        }
+    }
 };
 
 #endif
