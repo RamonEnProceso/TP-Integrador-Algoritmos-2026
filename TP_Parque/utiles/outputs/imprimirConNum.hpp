4ea2c3246ec0b4be5baa49bcaedf6b8ada9a7a3e
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-06 22:09:00 -0300

    feat: Añadir lógica para imprimir con número

---
+++ TP_Parque/utiles/outputs/imprimirConNum.hpp
@@ -0,0 +1,12 @@
+#ifndef IMPRIMIR_CON_NUMERO_H
+#define IMPRIMIR_CON_NUMERO_H
+#include <iostream>
+
+using namespace std;
+
+void imprimirConNumero(int n, char texto[]){
+    cout << n << ". " << texto << "\n" ;
+    return;
+};
+
+#endif
