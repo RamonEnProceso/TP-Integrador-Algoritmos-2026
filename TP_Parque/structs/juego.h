4008a27f8f661260eadb2e9656c81e064a83661d
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-07 05:06:08 -0300

    feat: Cambiar formato de variable de Tipo de juego

--- TP_Parque/structs/juego.h
+++ TP_Parque/structs/juego.h
@@ -2,12 +2,11 @@
 #define JUEGO_H
 
 const int LARGO_NOMBRE = 32;
-const int LARGO_TIPO = 32;
 
 struct Juego {
     int codigo;
     char nombre[LARGO_NOMBRE];
-    char tipo[LARGO_TIPO]; 
+    int tipo; 
     float alturaMinima;
     int capacidad;
     bool operativo;
