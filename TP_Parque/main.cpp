d72b380aca8cc6c86b52d6ccaf5e1c42ad0e52dd
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-08 04:54:35 -0300

    feat: Agregar creacion de lista de visitantes activos dentro del main

--- TP_Parque/main.cpp
+++ TP_Parque/main.cpp
@@ -1,6 +1,9 @@
 #include "menu/mostrarMenuPrincipal.h"
+#include "logica/nodo/crearListaNodo.h"
+#include "logica/nodo/nodoVisitante.h"
 
 int main(){
+    NodoVisitante *listaVisitantes = crearLista();
     mostrarMenuPrincipal();
     return 0;
 }
