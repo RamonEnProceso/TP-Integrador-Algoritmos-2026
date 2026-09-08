c55efc8e976ca200de0686ab1596b8809f8efda3
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-08 04:31:43 -0300

    feat: Añadir funcion para buscar Visitante Activo por numero de pulsera

---
+++ TP_Parque/logica/nodo/buscarVisitanteActivoPorPulsera.cpp
@@ -0,0 +1,12 @@
+#include "nodo/buscarVisitanteActivoPorPulsera.h"
+#include "logica/nodo/nodoVisitante.h"
+
+bool buscarVisitanteActivoPorPulsera(
+    NodoVisitante *lista, int pulseraNum){
+    NodoVisitante *actual = lista;
+    while (actual != nullptr && actual->visitante.pulsera != pulseraNum)
+    {
+        actual = actual->siguiente;
+    }
+    return actual != nullptr;
+}
---
+++ TP_Parque/logica/nodo/buscarVisitanteActivoPorPulsera.h
@@ -0,0 +1,7 @@
+#ifndef BUSCAR_VISITANTE_ACTIVO_POR_PULSERA_H
+#define BUSCAR_VISITANTE_ACTIVO_POR_PULSERA_H
+#include "logica/nodo/nodoVisitante.h"
+
+bool buscarVisitanteActivoPorPulsera(NodoVisitante *lista, int pulseraNum);
+
+#endif
