2e166fa2c798469bd0ef8ea40b2d5c1b8ed5f4a9
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-08 15:54:16 -0300

    feat: Añadir funcion reutilizable para imprimir datos de Visitantes

---
+++ TP_Parque/utiles/outputs/imprimirVisitante.cpp
@@ -0,0 +1,16 @@
+#include "utiles/outputs/imprimirVisitante.h"
+#include "utiles/outputs/imprimirTitulo.h"
+#include "structs/visitante.h"
+#include <iostream>
+
+using namespace std;
+
+void imprimirVisitante(Visitante datos){
+    imprimirTitulo("Datos del Visitante");
+    cout << "Nombre: " << datos.nombre << endl;
+    cout << "Numero de pulsera:" << datos.pulsera << endl;
+    cout << "Edad: " << datos.edad << endl;
+    cout << "Altura: " << datos.altura << endl;
+    cout << "\n";
+    return;
+};
---
+++ TP_Parque/utiles/outputs/imprimirVisitante.h
@@ -0,0 +1,7 @@
+#ifndef IMPRIMIR_VISITANTE_H
+#define IMPRIMIR_VISITANTE_H
+#include "structs/visitante.h"
+
+void imprimirVisitante(Visitante datos);
+
+#endif
