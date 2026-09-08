b1b9968b62824b5c7c048358d30dc027e3e61049
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-08 04:16:54 -0300

    feat: Añadir funcion para generar datos de prueba

---
+++ TP_Parque/logica/prueba/generarDatosPrueba.cpp
@@ -0,0 +1,47 @@
+#include "logica/prueba/generarDatosPrueba.h"
+#include "logica/archivo/crearArchivoJuegos.h"
+#include "logica/nodo/agregarVisitanteLista.h"
+#include "logica/nodo/nodoVisitante.h"
+#include "structs/juego.h"
+#include <iostream>
+
+using namespace std;
+
+void generarDatosPrueba(NodoVisitante *lista){
+    cout << "Generando datos...\n" ;
+    cout << "Generando visitantes...\n" ;
+
+    lista = agregarVisitanteLista({ 1000, "Ramón Ramirez", 23, 1.83f }, lista);
+    lista = agregarVisitanteLista({ 1001, "Joaquin Orsini",   23, 1.83f }, lista);
+    lista = agregarVisitanteLista({ 1002, "Ramiro Szrebka",   27, 1.79f }, lista);
+    lista = agregarVisitanteLista({ 1003, "Ignacio Segovia",  19, 1.71f }, lista);
+    lista = agregarVisitanteLista({ 1004, "Manuel Guevara",   34, 1.68f }, lista);
+    lista = agregarVisitanteLista({ 1005, "Sebastian Longo",  16, 1.65f }, lista);
+    lista = agregarVisitanteLista({ 1006, "Cristian Alvarez", 42, 1.76f }, lista);
+    lista = agregarVisitanteLista({ 1007, "Alexis Nogueira",  22, 1.80f }, lista);
+    lista = agregarVisitanteLista({ 1008, "Rigo Morel",       29, 1.74f }, lista);
+    lista = agregarVisitanteLista({ 1009, "Lucas More",       14, 1.55f }, lista);
+    
+    cout << "Cargando en memoria...\n" ;
+
+    cout << "Generando juegos...\n" ;
+    Juego listaJuegos[10];
+    cout << "Generando juegos mecanicos...\n" ;
+    listaJuegos[0] = {1, "Montana Rusa", 0, 1.40, 24, true};
+    listaJuegos[1] = {2, "Rueda de la Fortuna", 0, 1.20, 32, true};
+    listaJuegos[2] = {3, "Autitos Chocadores", 0, 1.10, 20, true};
+    listaJuegos[3] = {4, "Barco Pirata", 0, 1.30, 30, true};
+    listaJuegos[4] = {5, "Vuelta al Mundo", 0, 1.20, 24, true};
+    cout << "Generando juegos acuaticos...\n" ;
+    listaJuegos[5] = {6, "Tobogan Acuatico", 1, 1.30, 12, true};
+    listaJuegos[6] = {7, "Rio Salvaje", 1, 1.20, 16, true};
+    listaJuegos[7] = {8, "Pileta de Olas", 1, 1.10, 40, true};
+    cout << "Generando juegos infantiles...\n" ;
+    listaJuegos[8] = {9, "Calesita", 2, 0.80, 20, true};
+    listaJuegos[9] = {10, "Tazas Locas", 2, 0.90, 16, true};
+    cout << "Grabandolos en archivo binario...\n" ;
+    crearArchivoJuegos(&listaJuegos, 10);
+
+    return;
+
+};
