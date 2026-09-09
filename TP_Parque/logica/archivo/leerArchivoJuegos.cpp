5dd315ea879bfe2c6641db7e7d09698296a8d3a2
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-08 22:32:38 -0300

    feat: Añadir funcion para leer Archivo de juegos

---
+++ TP_Parque/logica/archivo/leerArchivoJuegos.cpp
@@ -0,0 +1,27 @@
+#include "logica/archivo/leerArchivoJuegos.h"
+#include "logica/archivo/rutasArchivos.h"
+#include "utiles/outputs/imprimirConNum.h"
+#include "utiles/outputs/imprimirTitulo.h"
+#include "structs/juego.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int leerArchivoJuegos(Juego juegos[], int n){
+    ifstream arch(rutaArchivoJuegos, ios::binary);
+    if (!arch){cerr<<"Error al leer el archvio juegos.dat"; return 1;}
+
+    Juego juegoLeido;
+
+    imprimirTitulo("Lista de Juegos");
+    while (arch.read((char*)&juegoLeido,sizeof(juegoLeido))){
+        imprimirConNumero(juegoLeido.codigo,juegoLeido.nombre);
+    }
+
+    cout << "Fin de la lista de juegos.\n\n";
+
+    arch.close();
+
+    return 0;
+}
---
+++ TP_Parque/logica/archivo/leerArchivoJuegos.h
@@ -0,0 +1,7 @@
+#ifndef LEER_ARCHIVO_JUEGOS_H
+#define LEER_ARCHIVO_JUEGOS_H
+#include "structs/juego.h"
+
+int leerArchivoJuegos(Juego juegos[], int n);
+
+#endif
