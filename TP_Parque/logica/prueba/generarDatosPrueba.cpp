4d9c336b47e76b9db83a75781bfb4a90ed6c4b3f
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-12 06:43:08 -0300

    fix: Agregar variación en datos de prueba

--- TP_Parque/logica/prueba/generarDatosPrueba.cpp
+++ TP_Parque/logica/prueba/generarDatosPrueba.cpp
@@ -21,24 +21,24 @@ void generarDatosPrueba(NodoVisitante*& lista){
     lista = agregarVisitanteLista({ 1007, "Alexis Nogueira",  22, 1.80f }, lista);
     lista = agregarVisitanteLista({ 1008, "Rigo Morel",       29, 1.74f }, lista);
     lista = agregarVisitanteLista({ 1009, "Lucas More",       14, 1.55f }, lista);
-    
+
     cout << "Cargando en memoria...\n" ;
 
     cout << "Generando juegos...\n" ;
     Juego listaJuegos[10];
     cout << "Generando juegos mecanicos...\n" ;
-    listaJuegos[0] = {1, "Montana Rusa", 0, 1.40, 24, true};
-    listaJuegos[1] = {2, "Rueda de la Fortuna", 0, 1.20, 32, true};
-    listaJuegos[2] = {3, "Autitos Chocadores", 0, 1.10, 20, true};
-    listaJuegos[3] = {4, "Barco Pirata", 0, 1.30, 30, true};
-    listaJuegos[4] = {5, "Vuelta al Mundo", 0, 1.20, 24, true};
+    listaJuegos[0] = {1, "Montana Rusa", 0, 1.40f, 24, true};
+    listaJuegos[1] = {2, "Rueda de la Fortuna", 0, 1.20f, 32, false};
+    listaJuegos[2] = {3, "Autitos Chocadores", 0, 1.10f, 20, false};
+    listaJuegos[3] = {4, "Barco Pirata", 0, 1.30f, 30, true};
+    listaJuegos[4] = {5, "Pendulo Vertigo", 0, 1.20f, 24, false};
     cout << "Generando juegos acuaticos...\n" ;
-    listaJuegos[5] = {6, "Tobogan Acuatico", 1, 1.30, 12, true};
-    listaJuegos[6] = {7, "Rio Salvaje", 1, 1.20, 16, true};
-    listaJuegos[7] = {8, "Pileta de Olas", 1, 1.10, 40, true};
+    listaJuegos[5] = {6, "Tobogan Acuatico", 1, 1.30f, 12, true};
+    listaJuegos[6] = {7, "Rio Salvaje", 1, 1.20f, 16, true};
+    listaJuegos[7] = {8, "Pileta de Olas", 1, 1.10f, 40, false};
     cout << "Generando juegos infantiles...\n" ;
-    listaJuegos[8] = {9, "Calesita", 2, 0.80, 20, true};
-    listaJuegos[9] = {10, "Tazas Locas", 2, 0.90, 16, true};
+    listaJuegos[8] = {9, "Calesita", 2, 0.80f, 20, true};
+    listaJuegos[9] = {10, "Tazas Locas", 2, 0.90f, 16, true};
     cout << "Grabandolos en archivo binario...\n" << endl ;
     crearArchivoJuegos(listaJuegos, 10);
 
