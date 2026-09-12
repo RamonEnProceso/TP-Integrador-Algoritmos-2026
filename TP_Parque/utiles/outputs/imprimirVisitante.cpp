474410f0ca17eb65c699ac8cee9dc7d95e4479d2
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-12 03:51:44 -0300

    fix: Arreglar espacios

--- TP_Parque/utiles/outputs/imprimirVisitante.cpp
+++ TP_Parque/utiles/outputs/imprimirVisitante.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void imprimirVisitante(Visitante datos){
     imprimirTitulo("Datos del Visitante");
     cout << "Nombre: " << datos.nombre << endl;
-    cout << "Numero de pulsera:" << datos.pulsera << endl;
+    cout << "Numero de pulsera: " << datos.pulsera << endl;
     cout << "Edad: " << datos.edad << endl;
     cout << "Altura: " << datos.altura << endl;
     cout << "\n";
