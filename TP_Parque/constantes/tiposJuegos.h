4b000e339485fda58caddce877303693c5b1f53f
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-13 03:17:50 -0300

    refactor: Mover de carpeta a las constantes de tiposJUeogs

---
+++ TP_Parque/constantes/tiposJuegos.h
@@ -0,0 +1,7 @@
+#ifndef TIPOS_JUEGOS_H
+#define TIPOS_JUEGOS_H
+
+const int TIPOS_JUEGOS_CANTIDAD  = 3;
+const char* TIPOS_JUEGOS[TIPOS_JUEGOS_CANTIDAD] = {"Mecanico","Acuatico","Infantil"};
+
+#endif
