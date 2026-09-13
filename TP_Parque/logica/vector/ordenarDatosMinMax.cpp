f2d2af4cb35f27bda6dd978b2e36495eca8219b6
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-13 08:19:31 -0300

    feat: Añadir funcion que ordena de menor a mayor 2 funciones

---
+++ TP_Parque/logica/vector/ordenarDatosMinMax.cpp
@@ -0,0 +1,15 @@
+#include "utiles/swap.h"
+
+int ordenarDatosMinMax(char **nombres, int *datos, int tamaño){
+    bool huboCambios = true;
+    for (int i = 0; i < tamaño-1 && huboCambios ;i++){
+        huboCambios = false;
+        for (int j = 0; j < tamaño - i-1; j++){
+            if (datos[j] > datos[j+1]){
+                swapInt(&datos[j], &datos[j+1]);
+                swapString(&nombres[j], &nombres[j+1]);
+                huboCambios = true;
+            }
+        }
+    }
+}
---
+++ TP_Parque/logica/vector/ordenarDatosMinMax.h
@@ -0,0 +1,6 @@
+#ifndef ORDENAR_DATOS_MINMAX_H
+#define ORDENAR_DATOS_MINMAX_H
+
+int ordenarDatosMinMax(char **nombres, int *datos, int tamaño);
+
+#endif
