b0e7428dc301bf536fb078a66e62b9ffd816d273
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-13 07:27:29 -0300

    feat: Añadir funcion para calcular total de la matriz ocupacion

--- TP_Parque/logica/matriz/totalOcupacion.cpp
+++ TP_Parque/logica/matriz/totalOcupacion.cpp
@@ -1,4 +1,4 @@
-#include "logica/matriz/totalOcupación.h"
+#include "logica/matriz/totalOcupacion.h"
 #include "logica/matriz/totalPorJuego.h"
 #include "structs/matrizJuegoxHoras.h"
 #include "utiles/matematica/sumaTotal.h"
