58cc7a415e80217753d40cbb3d606597d80cbfdd
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-17 15:00:49 -0300

    fix: Añadir nuevas variables

--- TP_Parque/structs/matrizJuegoxHoras.h
+++ TP_Parque/structs/matrizJuegoxHoras.h
@@ -1,10 +1,12 @@
 #ifndef MATRIZ_JUEGOXHORAS_H
 #define MATRIZ_JUEGOXHORAS_H
 #include "constantes/matrizConstantes.h"
+#include "structs/juego.h"
 struct MatrizJuegoXHoras {
     int matriz[MAX_JUEGOS][FRANJAS_HORARIAS];
-    int juegos;
     int franjas;
+    int cantJuegos;
+    Juego listaJuegos;
 };
 
 #endif
