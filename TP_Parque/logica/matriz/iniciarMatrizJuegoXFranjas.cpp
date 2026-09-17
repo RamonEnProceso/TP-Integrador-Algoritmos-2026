68ab7101e6eb7025567256e48ee3bb6d484394af
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-17 15:29:47 -0300

    feat: Añadir funcion para inicializar Matriz

---
+++ TP_Parque/logica/matriz/iniciarMatrizJuegoXFranjas.cpp
@@ -0,0 +1,18 @@
+#include "structs/matrizJuegoxHoras.h"
+#include "constantes/matrizConstantes.h"
+#include "structs/juego.h"
+#include <iostream>
+
+using namespace std;
+
+void iniciarMatrizJuegoXFranjas (MatrizJuegoXHoras datos, Juego juegos[]){
+    cout << "Inicializar datos de opación de juegos por franja horaria...\n\n";
+    for (int i = 0; i<datos.cantJuegos;i++){
+        cout<< "Cargando visitas al juego: " << juegos[i].nombre;
+        for (int j = 0; i<datos.franjas; j++){
+            cout << "Cargando visitas de " << FRANJAS_TEXTO[j] <<": ";
+            cin >> datos.matriz[i][j]; cout << endl;
+        }
+    }
+    cout << "Datos de visitas registrados correctamente.\n\n";
+}
---
+++ TP_Parque/logica/matriz/iniciarMatrizJuegoXFranjas.h
@@ -0,0 +1,8 @@
+#ifndef INICIAR_MATRIZ_JUEGOXFRANJAS_H
+#define INICIAR_MATRIZ_JUEGOXFRANJAS_H
+#include "structs/matrizJuegoxHoras.h"
+#include "constantes/matrizConstantes.h"
+
+void iniciarMatrizJuegoXFranjas (MatrizJuegoXHoras datos, Juego juegos[]);
+
+#endif
