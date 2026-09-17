e799bfa2b0fa8dd0c63cf4753d1e629c091db339
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-17 18:48:36 -0300

    fix: Añadir soporte por si no existen visitantes activos

--- TP_Parque/logica/nodo/mostrarVisitantesActivos.cpp
+++ TP_Parque/logica/nodo/mostrarVisitantesActivos.cpp
@@ -10,9 +10,15 @@ using namespace std;
 void mostrarVisitantesActivos(NodoVisitante *lista){
     NodoVisitante *actual = lista;
     imprimirTitulo("Lista de Visitantes Activos");
+    int contador = 0;
     while (actual != nullptr){
+        contador++;
         imprimirConNumero(actual->visitante.pulsera, actual->visitante.nombre);
         actual = actual->siguiente;
     }
-    cout<<"Fin de la lista.\n\n";
+    if(contador=0){
+        cout<<"No se encontraron visitantes.\n\n";
+    }else{
+        cout<<"Fin de la lista.\n\n";
+    }
 }
