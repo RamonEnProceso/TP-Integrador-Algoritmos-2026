d49ecacad55494eedeb88e70ad54b659abd5eba5
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-17 18:04:40 -0300

    feat: Añadir funcion para mostrar lista de juegos

--- TP_Parque/logica/vector/mostrarListaJuegos.cpp
+++ TP_Parque/logica/vector/mostrarListaJuegos.cpp
@@ -1,12 +1,15 @@
 #include "structs/juego.h"
 #include "utiles/outputs/imprimirConNum.h"
+#include "utiles/outputs/imprimirTitulo.h"
 #include <iostream>
 
 using namespace std;
 
 void mostrarListaJuegos(Juego *listaJuegos, int cantJuegos){
+    imprimirTitulo("Lista de Juegos");
     if(cantJuegos < 1){cout<<"No hay juegos en lista.\n";}
     for (int i; i<cantJuegos; i++){
         imprimirConNumero(i,listaJuegos[i].nombre);}
+    cout <<"Fin de lista de Juegos.\n\n";
     return;
 }
