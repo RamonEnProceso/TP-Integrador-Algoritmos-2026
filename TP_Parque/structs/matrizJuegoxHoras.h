c8b3e43786ff81c32d6b153eaca8356d9bc4b700
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-17 13:20:28 -0300

    fix: Hardcodear los límites de la matriz

--- TP_Parque/structs/matrizJuegoxHoras.h
+++ TP_Parque/structs/matrizJuegoxHoras.h
@@ -1,8 +1,8 @@
 #ifndef MATRIZ_JUEGOXHORAS_H
 #define MATRIZ_JUEGOXHORAS_H
-
+#include "constantes/matrizConstantes.h"
 struct MatrizJuegoXHoras {
-    int **matriz;
+    int matriz[MAX_JUEGOS][FRANJAS_HORARIAS];
     int juegos;
     int franjas;
 };
