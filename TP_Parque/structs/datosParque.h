a5f46470d17c7ccefc7ecf89d7ba4abb89d7dc63
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-17 16:27:04 -0300

    feat: Crear estructura para transferir datos con facilidad

---
+++ TP_Parque/structs/datosParque.h
@@ -0,0 +1,13 @@
+#ifndef DATOS_PARQUE_H
+#define DATOS_PARQUE_H
+#include "structs/matrizJuegoxHoras.h"
+#include "structs/nodoVisitante.h"
+#include "structs/juego.h"
+
+struct DatosParque {
+    NodoVisitante* listaVisitantes;
+    MatrizJuegoXHoras matriz;
+    Juego juegos[MAX_JUEGOS];
+};
+
+#endif
