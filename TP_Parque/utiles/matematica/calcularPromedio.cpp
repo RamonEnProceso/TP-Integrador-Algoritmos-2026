da3a5e120583e468edda412822eade81e3708c4c
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-20 09:23:11 -0300

    refactor: Eliminar funcion nunca utilizada

--- TP_Parque/utiles/matematica/calcularPromedio.cpp
+++
@@ -1,7 +0,0 @@
-#include "utiles/matematica/calcularPromedio.h"
-#include "utiles/matematica/sumaTotal.h"
-
-int calcularPromedio(int vec[], int n){
-    int total = sumaTotalV(vec, n);
-    return total/n;
-};
--- TP_Parque/utiles/matematica/calcularPromedio.h
+++
@@ -1,6 +0,0 @@
-#ifndef CALCULAR_PROMEDIO_H
-#define CALCULAR_PROMEDIO_H
-
-int calcularPromedio(int vec[], int n);
-
-#endif
